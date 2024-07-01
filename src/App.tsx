eba38b766718bd94dd8d2ae52f761e03a7791b9f
josephorng <josephorng@hotmail.com>
2024-07-02 07:08:52 +0800

    try to make storage work

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,37 @@
+import React from "react";
+import { uploadData } from "aws-amplify/storage";
+
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-// import { uploadData } from "aws-amplify/storage";
-
-const client = generateClient<Schema>();
 
 function App() {
-  // const [file, setFile] = React.useState<File | null>(null);
+  const [file, setFile] = React.useState();
 
-  // const handleChange = (event: any) => {
-  //   if (event.target.files && event.target.files[0]) {
-  //     setFile(event.target.files[0]);
-  //   }
-  // };
-
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
+  const handleChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
 
   return (
     <Authenticator>
       {({ signOut }) => (
         <main>
-          <h1>My todos</h1>
-          <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li onClick={() => deleteTodo(todo.id)} key={todo.id}>
-                {todo.content}
-              </li>
-            ))}
-          </ul>
           <div>
-            🥳 App successfully hosted. Try creating a new todo.
-            <br />
-            <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-              Review next step of this tutorial.
-            </a>
-          </div>
-          {/* <div>
             <input type="file" onChange={handleChange} />
             <button
-              onClick={() => {
-                if (file) {
-                  uploadData({
-                    path: `picture-submissions/${file.name}`,
-                    data: file,
-                  });
-                } else {
-                  alert("Please select a file first");
-                }
-              }}
+              onClick={() =>
+                uploadData({
+                  path: `photos/${file.name}`,
+                  data: file,
+                })
+              }
             >
               Upload
             </button>
-          </div> */}
+          </div>
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
     </Authenticator>
   );
 }
-
 export default App;
