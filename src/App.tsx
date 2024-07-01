a483328f02b045123ddbdffbeefc046394c94a5d
josephorng <josephorng@hotmail.com>
2024-07-01 21:42:24 +0800

    try to fix login error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { uploadData } from "aws-amplify/storage";
 
 const client = generateClient<Schema>();
 
 function App() {
+  const [file, setFile] = React.useState<File | null>(null);
+
+  const handleChange = (event: any) => {
+    if (event.target.files && event.target.files[0]) {
+      setFile(event.target.files[0]);
+    }
+  };
+
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -27,7 +36,7 @@ function App() {
     <Authenticator>
       {({ signOut }) => (
         <main>
-          <h1>My todos</h1>
+          {/* <h1>My todos</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
@@ -42,6 +51,23 @@ function App() {
             <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
               Review next step of this tutorial.
             </a>
+          </div> */}
+          <div>
+            <input type="file" onChange={handleChange} />
+            <button
+              onClick={() => {
+                if (file) {
+                  uploadData({
+                    path: `picture-submissions/${file.name}`,
+                    data: file,
+                  });
+                } else {
+                  alert("Please select a file first");
+                }
+              }}
+            >
+              Upload
+            </button>
           </div>
           <button onClick={signOut}>Sign out</button>
         </main>
